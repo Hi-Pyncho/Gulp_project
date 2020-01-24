a8cb31869c521926162369cb9adc5fb738172830
Hi-Pyncho <garrakombo@gmail.com>
2020-01-24 15:01:59 +0300

    добавил пару модулей

--- gulpfile.js
+++ gulpfile.js
@@ -1,120 +1,124 @@
 //Подключаем модули галпа
 const gulp = require('gulp');
-//объединение файлов
+//Объединение файлов
 const concat = require('gulp-concat');
-//добавление префиксов
+//Добапвление префиксов
 const autoprefixer = require('gulp-autoprefixer');
-//оптимизация стилей
+//Оптимизация стилей
 const cleanCSS = require('gulp-clean-css');
-// оптимизация скриптов
+//Оптимизация скриптов
 const uglify = require('gulp-uglify');
-//удаление файлов
+//Удаление файлов
 const del = require('del');
-//синхронизация с браузером
+//Синхронизация с браузером
 const browserSync = require('browser-sync').create();
-//компилятор SASS
+//Для препроцессоров стилей
+const sourcemaps = require('gulp-sourcemaps');
+//Sass препроцессор
 const sass = require('gulp-sass');
-//для препроцессоров стилей
-const sourceMap = require('gulp-sourcemaps');
-// для остлеживания изменений
-const Gulpwatch = require('gulp-watch')
-// для оптимизации изображений
+//Less препроцессор
+const less = require('gulp-less');
+//Stylus препроцессор
+const stylus = require('gulp-stylus');
+//Модуль для сжатия изображений
 const imagemin = require('gulp-imagemin');
+//Модуль переименовывания файлов
+const rename = require('gulp-rename');
 
-//порядок подключения css стилей
+//Порядок подключения файлов со стилями
 const styleFiles = [
-    './src/css/main.scss',
-    './src/css/media.scss'
+   './src/css/color.scss',
+   './src/css/main.scss'
 ]
-
-//порядок подключения js скриптов
+//Порядок подключения js файлов
 const scriptFiles = [
-    './src/js/lib.js',
-    './src/js/main.js'
+   './src/js/lib.js',
+   './src/js/main.js'
 ]
-
 
 //Таск для обработки стилей
 gulp.task('styles', () => {
-    //Шаблон для поиска файлов CSS
-    //Всей файлы по шаблону './src/css/**/*.css'
-    return gulp.src(styleFiles)
-       .pipe(sourceMap.init())
-       //Указать stylus() , sass() или less()
-       .pipe(sass())
-       //Объединение файлов в один
-       .pipe(concat('style.css'))
-       //Добавить префиксы
-       .pipe(autoprefixer({
+   //Шаблон для поиска файлов CSS
+   //Всей файлы по шаблону './src/css/**/*.css'
+   return gulp.src(styleFiles)
+      .pipe(sourcemaps.init())
+      //Указать stylus() , sass() или less()
+      .pipe(stylus())
+      //Объединение файлов в один
+      .pipe(concat('style.css'))
+      //Добавить префиксы
+      .pipe(autoprefixer({
          overrideBrowserslist: ['last 2 versions'],
-          cascade: false
-       }))
-       //минифицируем CSS файл (ур 2 - жесткая минификация)
-       .pipe(cleanCSS({
-          level: 2
-       }))
-       .pipe(sourceMap.write('./'))
-       //Выходная папка для стилей
-       .pipe(gulp.dest('./build/css'))
+         cascade: false
+      }))
+      //Минификация CSS
+      .pipe(cleanCSS({
+         level: 2
+      }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(rename({
+         suffix: '.min'
+      }))
+      //Выходная папка для стилей
+      .pipe(gulp.dest('./build/css'))
        //для обновления стилей на сайте
-       .pipe(browserSync.stream());
- });
-
-
+      .pipe(browserSync.stream());
+});
 
 //Таск для обработки скриптов
 gulp.task('scripts', () => {
-    //Шаблон для поиска файлов JS
-    //Всей файлы по шаблону './src/js/**/*.js'
-    return gulp.src(scriptFiles)
-       //Объединение файлов в один
-       .pipe(concat('script.js'))
-       //минифицируем файл JS + сокращает имена(труднее читать)
+   //Шаблон для поиска файлов JS
+   //Всей файлы по шаблону './src/js/**/*.js'
+   return gulp.src(scriptFiles)
+      //Объединение файлов в один
+      .pipe(concat('main.js'))
+      //минифицируем файл JS + сокращает имена(труднее читать)
        // {toplevel: true} - в скобках //макс ур-нь миниф-ии. сокр-т названия переменных//Минификация JS
-       .pipe(uglify({
-          toplevel: true
-       }))
-       //Выходная папка для скриптов
-       .pipe(gulp.dest('./build/js'))
-       //для обновления скриптов на сайте   
-       .pipe(browserSync.stream());
- });
- 
- //Таск для очистки папки build
- gulp.task('del', () => {
-    return del(['build/*'])
- });
- 
-// таск для оптимизации изображений
-gulp.task('img-compress', () => {
+      .pipe(uglify({
+         toplevel: true
+      }))
+      .pipe(rename({
+         suffix: '.min'
+      }))
+      //Выходная папка для скриптов
+      .pipe(gulp.dest('./build/js'))
+       //для обновления скриптов на сайте
+      .pipe(browserSync.stream());
+});
+
+//Таск для очистки папки build
+gulp.task('del', () => {
+   return del(['build/*'])
+});
+
+//Таск для сжатия изображений
+gulp.task('img-compress', ()=> {
    //любые вложенности в img
    return gulp.src('./src/img/**')
-   .pipe(imagemin( {
+   .pipe(imagemin({
       progressive: true
    }))
    // тут сохранятся эти вложенности, то есть сохраняется изначальная структура папок
    .pipe(gulp.dest('./build/img/'))
-})
+});
 
- //Таск для отслеживания изменений в файлах
- gulp.task('watch', () => {
-    browserSync.init({
-       server: {
-          baseDir: "./"
-       }
-    });
-    // следить за изменениями папок с изобаржениями
-    gulp.watch('./src/img/**', gulp.series('img-compress'))
-    //Следить за файлами со стилями с нужным расширением
-    gulp.watch('./src/css/**/*.scss', gulp.series('styles'))
-    //Следить за JS файлами
-    gulp.watch('./src/js/**/*.js', gulp.series('scripts'))
-    //При изменении HTML запустить синхронизацию
-    gulp.watch("./*.html").on('change', browserSync.reload);
- });
- 
- //Таск по умолчанию, Запускает del, styles, scripts и watch
- // default - название по умолчанию - то есть можно запускать этот таск просто введя gulp
- gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts', 'img-compress'), 'watch'));
+//Таск для отслеживания изменений в файлах
+gulp.task('watch', () => {
+   browserSync.init({
+      server: {
+         baseDir: "./"
+      }
+   });
+   //Следить за добавлением новых изображений
+   gulp.watch('./src/img/**', gulp.series('img-compress'))
+   //Следить за файлами со стилями с нужным расширением
+   gulp.watch('./src/css/**/*.scss', gulp.series('styles'))
+   //Следить за JS файлами
+   gulp.watch('./src/js/**/*.js', gulp.series('scripts'))
+   //При изменении HTML запустить синхронизацию
+   gulp.watch("./*.html").on('change', browserSync.reload);
+});
 
-
+//Таск по умолчанию, Запускает del, styles, scripts, img-compress и watch
+// default - название по умолчанию - то есть можно запускать этот таск просто введя gulp
+gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts', 'img-compress'), 'watch'));
